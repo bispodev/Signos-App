d67c7826bdb57a1e5f8b56799d1f379df576dcf1
João Gustavo <57817224+bispodev@users.noreply.github.com>
2020-03-11 14:21:05 -0300

    Preechimendo dos Spinners e Layout
    Preenchendo Spinners e mudança no layout

--- app/src/main/java/com/bispodev/mysignos/MainActivity.java
+++ app/src/main/java/com/bispodev/mysignos/MainActivity.java
@@ -3,12 +3,36 @@ package com.bispodev.mysignos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner sn_month;
+    Spinner sn_day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fillSpinner();
+
+    }
+
+
+    private void fillSpinner(){
+
+        sn_day = findViewById(R.id.sn_day);
+        sn_month = findViewById(R.id.sn_month);
+
+        ArrayAdapter<CharSequence> adapter_day = ArrayAdapter.createFromResource(this, R.array.array_day, R.layout.support_simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adapter_month = ArrayAdapter.createFromResource(this, R.array.array_month, R.layout.support_simple_spinner_dropdown_item);
+
+        adapter_day.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        adapter_month.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        sn_day.setAdapter(adapter_day);
+        sn_month.setAdapter(adapter_month);
     }
 }
